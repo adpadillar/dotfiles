74f5bfc07b0b4dd748ca8fe079a28d74d045a2ac
Axel Padilla <adpadillar25@gmail.com>
2023-01-16 15:13:54 -0700

    Added git alias

--- git/setup.sh
+++ git/setup.sh
@@ -1,4 +1,4 @@
-# .gitconfig name
+# .gitconfig name and email
 git config --global user.name "Axel Padilla"
 git config --global user.email "adpadillar25@gmail.com"
 
@@ -7,12 +7,9 @@ GPG_USER_ID="Axel Padilla <adpadillar25@gmail.com>"
 GPG_ALGO="rsa4096"
 GPG_USAGE="default"
 GPG_EXPIRE="3m"
-GPG_KEY_ID=""
 
 # Generate key
-gpg --quick-gen-key $GPG_USER_ID $GPG_ALGO $GPG_USAGE $GPG_EXPIRE >> git_setup_temp.txt
-GPG_KEY_ID=$(sed '2!d' git_setup_temp.txt | tr ' ' '\0')
-rm git_setup_temp.txt
+gpg --quick-gen-key $GPG_USER_ID $GPG_ALGO $GPG_USAGE $GPG_EXPIRE
 
 # Export key
 echo "Add this key to: https://github.com/settings/gpg/new \n"
@@ -22,11 +19,14 @@ gpg --armor --export $GPG_USER_ID
 git config --global user.signingkey $GPG_USER_ID
 git config --global commit.gpgsign true
 
+# Alias .zshrc
+echo 'alias git="GPG_TTY=$(tty) git"' >> $HOME/.zshrc
+
 # You need to configure ssh keys for cloning
 mkdir -p $HOME/.ssh
-ssh-keygen -N "" -f "$HOME/.ssh/github_rsa"
+ssh-keygen -N "" -f "$HOME/.ssh/id_rsa"
 echo "Add this key to: https://github.com/settings/ssh/new, don't copy the headers"
 
 echo "--- BEGIN SSH KEY ---"
-cat $HOME/.ssh/github_rsa.pub 
+cat $HOME/.ssh/id_rsa.pub 
 echo "--- END SSH KEY ---"
