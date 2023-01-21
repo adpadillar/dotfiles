421d73a2370df99590a54986e10441bc7668d023
Axel Padilla <adpadillar25@gmail.com>
2023-01-21 09:44:17 -0700

    added android studio

---
+++ android-studio/install.sh
@@ -0,0 +1,4 @@
+brew install android-studio
+echo "export ANDROID_SDK=$HOME/Library/Android/sdk" >> $HOME/.zprofile
+echo "export PATH=$HOME/Library/Android/sdk/platform-tools:$PATH" >> $HOME/.zprofile
+source $HOME/.zprofile
