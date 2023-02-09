db033dafd367462bb2bcf10ecbebad8cb142e0b8
Axel Padilla <adpadillar25@gmail.com>
2023-02-09 10:55:19 -0700

    added rust to .zshrc

--- rust/install.sh
+++ rust/install.sh
@@ -1,4 +1,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >> rust.sh
 sh rust.sh -y
 source "$HOME/.cargo/env"
+echo "" >> "$HOME/.zshrc"
+echo '# ADDING RUST TO PATH' >> "$HOME/.zshrc"
+echo 'source "$HOME/.cargo/env"' >> "$HOME/.zshrc"
 rm rust.sh
