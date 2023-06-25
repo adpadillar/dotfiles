fa82b71034aaae3d3c93c5207296ed6012da703e
Axel Padilla <adpadillar25@gmail.com>
2023-06-25 17:37:50 -0600

    fixing nvim

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,6 +1,8 @@
-NVIM_PATH="$HOME/.config/nvim"
+CONFIG_PATH="$HOME/.config"
+NVIM_PATH="$CONFIG_PATH/nvim"
 # Remove .config/nivm
 rm -rf $NVIM_PATH
+mkdir -p $CONFIG_PATH
 
 # Symlink
-ln -s $PWD/nvim/ $HOME/.config
+ln -s $PWD/nvim $HOME/.config/nvim
