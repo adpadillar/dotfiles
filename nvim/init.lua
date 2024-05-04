4915c5d5d5195e254a6aceff63ee63313bfde4d6
Axel Padilla <adpadillar25@gmail.com>
2024-05-04 14:34:18 -0600

    changes to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -77,6 +77,8 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  -- Copilot (hopefully)
+  'github/copilot.vim',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
